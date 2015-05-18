357b6909985663bd7610cda22ef18ac6903478d3
Alexander Böhn <fish2000@gmail.com>
2015-05-18 17:01:55 -0400

    Testing skeleton

--- test/test_rolodexer.py
+++ test/test_rolodexer.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python
+
+import unittest
+
+sample_input = """
+Booker T., Washington, 87360, 373 781 7380, yellow
+Chandler, Kerri, (623)-668-9293, pink, 123123121
+James Murphy, yellow, 83880, 018 154 6474
+asdfawefawea
+""".strip()
+
+sample_output = u"""
+{
+  "entries": [
+    {
+      "color": "yellow",
+      "firstname": "James",
+      "lastname": "Murphy",
+      "phonenumber": "018-154-6474",
+      "zipcode": "83880"
+    },
+    {
+      "color": "yellow",
+      "firstname": "Booker T.",
+      "lastname": "Washington",
+      "phonenumber": "373-781-7380",
+      "zipcode": "87360"
+    }
+  ],
+  "errors": [
+    1,
+    3
+  ]
+}
+""".strip()
+
+class RolodexerTest(unittest.TestCase):
+    pass
+    
